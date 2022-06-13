f8e3bc32877c6be3bb2d294834cdf9b532494e12
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-14 01:22:22 +0300

    feat: добавить модель movie

---
+++ models/Movie.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+const configURLValidator = require('./utils/configURLValidator')
+
+const movieScheme = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+
+  director: {
+    type: String,
+    required: true,
+  },
+
+  duration: {
+    type: Number,
+    required: true,
+  },
+
+  year: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(imageURL) {
+        return validator.isURL(imageURL, configURLValidator)
+      }
+    },
+  },
+
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(imageURL) {
+        return validator.isURL(imageURL, configURLValidator)
+      }
+    },
+  },
+
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(imageURL) {
+        return validator.isURL(imageURL, configURLValidator)
+      }
+    },
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+
+  movieId: {
+    required: true,
+  },
+
+  nameRU: {
+    type: String,
+    required: true,
+  },
+
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieScheme);
---
+++ utils/configURLValidator.js
@@ -0,0 +1,3 @@
+module.exports = {
+  require_protocol: true,
+}
