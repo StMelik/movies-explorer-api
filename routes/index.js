1a3f63e6b431d30245a36403019fd6477d373eb4
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-14 23:43:11 +0300

    feat: добавить 404 роут

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRouter = require('./users.router');
 const movieRouter = require('./movies.router');
+const notFoundRouter = require('./404.router');
 const { createUser, login } = require('../controllers/users.controller');
 const auth = require('../middlewares/auth.middleware');
 const { validationUser, validationLogin } = require('../utils/validation');
@@ -10,5 +11,7 @@ router.post('/signin', validationLogin, login);
 
 router.use('/users', auth, userRouter);
 router.use('/movies', auth, movieRouter);
+
+router.use('/', auth, notFoundRouter);
 
 module.exports = router;
