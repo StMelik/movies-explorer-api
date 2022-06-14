2e4a7556ad3ae905517d7f4391e150d6aa88caec
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-14 07:33:04 +0300

    feat: добавить логирование

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const error = require('./middlewares/error.middleware');
+const { requestLogger, errorLogger } = require('./middlewares/logger.middleware')
 
 const { PORT = 3000 } = process.env;
 
@@ -12,7 +13,11 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
+app.use(requestLogger)
+
 app.use(routes);
+
+app.use(errorLogger)
 
 app.use(error);
 
